5a5968d6943ada7e1562296511c02624df1824e4
Xavier OILLIC <xavieroillic@gmail.com>
2023-11-10 12:07:28 +0100

    le gros du responsive est ok

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ fetch(urlApi)                    // Fetch cherche sur le WEB.
     console.log(jsonBanana);
     //return jsonBanana;
 
-        // Je STOCKE les donnnées recues de grâce à l'API dans la CONST ci nommée
+        // Je STOCKE les donnnées recues grâce à l'API dans la CONST ci nommée
 const pays = jsonBanana.city_info.country;
-const ville = jsonBanana.city_info.name;
+//const ville = jsonBanana.city_info.name;
 const latitude = jsonBanana.city_info.latitude;
 const longitude = jsonBanana.city_info.longitude;
 const temp = jsonBanana.current_condition.tmp;
@@ -19,26 +19,33 @@ const vent = jsonBanana.current_condition.wnd_spd;
 
 // Je STOCKE dans la variable ci nommée le lieu ou je veux afficher les données.
 const iciPays = document.getElementById("country_place");
-const iciVille = document.getElementById("city_place");
+//const iciVille = document.getElementById("city_place");
 const iciLatitude = document.getElementById("latitude_place");
 const iciLongitude = document.getElementById("longitude_place");
 const iciTemp = document.getElementById("temp_place");
 const iciWind = document.getElementById("wind_place");
 
 // Je dis que les données de l'API vont aller dans ICI... grace à innertext.
-iciPays.innerText = `Nous sommes en : ${pays}`;
-iciVille.innerText = `Nous sommes à : ${ville}`;
+iciPays.innerText = `${pays}`;
+//iciVille.innerText = `Nous sommes à : ${ville}`;
 iciLatitude.innerText = `En haut, ici... "${latitude}" de Latitude nord.`;
 iciLongitude.innerText = `Par ici, de ce coté... "${longitude}" de Longitude Est.`;
 iciTemp.innerText = `Chaud ou Froid... ${temp} Celcius`;
-iciWind.innerText = `venteux... ${vent} km/h`;
+iciWind.innerText = ` ${vent} km/h`;
 
 });
 
 
-//const AffichMeteo = () => {
-//}
-//AffichMeteo()
+function ChoisirVille(event) {
+event.preventDefault()
+
+//Je stocke la VALEUR SAISIE par le visiteur.
+const villeRecherchee = new Text(document.querySelector("#input_choice").value);
+console.log(villeRecherchee);
 
 
+//Je Stocke dans cette var la Saisie du visiteur.
+//return document.getElementById('#city').innerHTML = `${iciVille}`
+}
+ChoisirVille()
 
